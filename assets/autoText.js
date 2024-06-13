75366a4096968b3b1ab369d63de99ccb0fbb485b
Carlos Eduardo da Silva Santos <carloseduardodass76@gmail.com>
2024-06-12 21:00:51 -0300

    att js, css, form and assets

---
+++ assets/autoText.js
@@ -0,0 +1,8 @@
+function autoText(){
+    var typed = new Typed('#auto-text', {
+        strings: ['<h3>Conectando mentes inovadoras e impulsionando o futuro das startups.</h3>', '<h3>Conosco cada conexão é uma porta para o progresso.</h3>','<h3>Aproveite a vida com a InovaNetwork, juntos construindo o amanhã da inovação.</h3>'],
+        typeSpeed: 50,
+        showCursor: false
+    })    
+}
+autoText()
