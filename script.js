23e2d8549f1cdc78991e65c318ac54c981a85a29
higor kaique <h1.gorr3210@gmail.com>
2024-06-24 20:47:30 -0300

    abu

--- script.js
+++ script.js
@@ -1,31 +1,4 @@
 
-// Função para exibir o modal de contatos
-function displayContactModal() {
-    // Elementos do modal
-    let modal = document.getElementById('modal');
-    let closeButton = document.getElementsByClassName('close')[0];
-
-    // Evento de clique para exibir o modal quando o link "Contatos" é clicado
-    document.getElementById('contact-link').addEventListener('click', function() {
-        modal.style.display = 'block';
-    });
-
-    // Evento de clique para fechar o modal quando o botão de fechar é clicado
-    closeButton.addEventListener('click', function() {
-        modal.style.display = 'none';
-    });
-
-    // Evento de clique fora do modal para fechar
-    window.addEventListener('click', function(event) {
-        if (event.target == modal) {
-            modal.style.display = 'none';
-        }
-    });
-}
-
-// Chama a função para exibir o modal de contatos
-displayContactModal();
-
 // AutoType
 function autoText(){
     var typed = new Typed('#auto-text', {
