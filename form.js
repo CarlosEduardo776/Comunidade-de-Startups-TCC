6439d38f5d5ee90b00527be37ed58eba06dd3d92
higor kaique <h1.gorr3210@gmail.com>
2024-06-27 21:43:02 -0300

    aby (Carlos e Higor)

--- form.js
+++ form.js
@@ -8,3 +8,51 @@ document.getElementById('cpf').addEventListener('input', function (e) {
     e.target.value = cpfPattern;
 });
 
+const telefone = document.getElementById('telefone');
+
+// adiciona listener para o evento input
+telefone.addEventListener('input', function() {
+    // remove qualquer caractere que não seja digito numerico
+    let cleanedValue = this.value.replace (/\D/g, '');
+
+    // limita o comprimento maximo
+    if (cleanedValue.length > 11) {
+            cleanedValue = cleanedValue.slice(0, 11);
+    }
+
+// formatação dos números
+let formattedValue = '';
+
+// Insere os primeiros dois primeiros números entre parenteses
+if (cleanedValue.length >= 2) {
+    formattedValue += '(' + cleanedValue.substring(0, 2) + ')';
+}
+else if (cleanedValue.length > 0) {
+    formattedValue += '(' + cleanedValue.substring(0, cleanedValue.length);
+}
+
+// adiciona o hifen depois de 5 digitos
+if (cleanedValue.length > 7) {
+    formattedValue += '-' + cleanedValue.substring(7);
+}
+
+// define o valor formatado no input
+this.value = formattedValue;
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
